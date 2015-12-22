9e6e2749c43018c780b86b1d65d344c72c2a9706
Suhan Koh <suhankoh@gmail.com>
2015-12-21 20:43:05 -0800

    AngularJs is working, but still need to work on CSS at project page.

--- js/angularControllers/projects.js
+++ js/angularControllers/projects.js
@@ -1,14 +1,14 @@
-(function(){
-  var projects = [
-  { name: 'Bomberman Clone', description: "test", image: './css/images/projects/sample.jpg'},
-  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'}
-  ];
-
-
-  var app = angular.module('SuhanProject', []);
+(function() {
+  var app = angular.module('SuhanProjects', []);
 
   app.controller('ProjectController', function(){
-    this.product = projects
+    this.projects = projectLists;
   });
 
+  var projectLists = [
+  { name: 'Bomberman Clone', description: "test", image: './css/images/projects/sample.jpg'},
+  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
+  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'}
+
+  ];
 })();

75e33eb1a2ebb30b52b78f4f9ecca4e7d5c2e3bd
Suhan Koh <suhankoh@gmail.com>
2015-12-22 02:30:26 -0800

    Fix some issue with css

--- js/angularControllers/projects.js
+++ js/angularControllers/projects.js
@@ -8,6 +8,9 @@
   var projectLists = [
   { name: 'Bomberman Clone', description: "test", image: './css/images/projects/sample.jpg'},
   { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
+  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
+  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
+  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
   { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'}
 
   ];

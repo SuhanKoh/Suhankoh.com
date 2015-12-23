06b87ce8cff8ebb6cee65eaec1f830c24a821351
Suhan Koh <suhankoh@gmail.com>
2015-12-22 17:35:11 -0800

    Cleaned up project page and added informations.

--- js/angularControllers/projects.js
+++ js/angularControllers/projects.js
@@ -6,10 +6,10 @@
   });
 
   var projectLists = [
-  { name: 'Bomberman Clone', description: "test", image: './css/images/projects/sample.jpg'},
-  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
-  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
-  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'}
+  { name: 'Tank vs Tank', description: "A Java 2d sprite game built on swing", image: './css/images/projects/tankvstank.png', url: 'https://github.com/SuhanKoh/Tank_game'},
+  { name: 'Simple Calculator', description: "A simple calculator app wrote in Java", image: './css/images/projects/simplecalculator.png', url: 'https://github.com/SuhanKoh/SimpleCalculator'},
+  { name: 'Bomberman Clone', description: "A bomberman clone website written with Ruby on Rails MVC.", image: './css/images/projects/bombermanclone.png', url: 'http://team-b.herokuapp.com'},
+  { name: 'Encryption modes', description: "DES Encryption modes, included CBC, ECB, ORB etc", image: './css/images/projects/java.png', url: 'https://github.com/SuhanKoh/DESEncryption'}
 
   ];
 })();

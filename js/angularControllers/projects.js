ce46e6cf11c8d6d6f6d8f4f5ef860cc0f5befd6a
Suhan Koh <suhankoh@gmail.com>
2015-12-22 17:11:40 -0800

    Fixed some more css settings.

--- js/angularControllers/projects.js
+++ js/angularControllers/projects.js
@@ -9,8 +9,6 @@
   { name: 'Bomberman Clone', description: "test", image: './css/images/projects/sample.jpg'},
   { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
   { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
-  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
-  { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'},
   { name: 'Bomberman Clone2', description: "test2", image: './css/images/projects/sample.jpg'}
 
   ];

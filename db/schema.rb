8a34b5c182b1c7f388484cbf7a0b3da3e070699f
Suhan Koh <suhankoh@gmail.com>
2016-06-03 03:10:56 -0700

    Dropped sample table schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603040657) do
+ActiveRecord::Schema.define(version: 20160603101002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

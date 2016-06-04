6441e873304ce5e610815bed173654639d331b40
Suhan Koh <suhankoh@gmail.com>
2016-06-03 19:11:35 -0700

    Added bloggo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  Blogo::Routes.mount_to(self, at: '/blog')
+
   resources :pages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -6,7 +8,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root '#index'
   root 'pages#index'
-
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,285 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603101002) do
+ActiveRecord::Schema.define(version: 20160604012634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "blogo_posts", force: :cascade do |t|
+    t.integer  "user_id",          null: false
+    t.string   "permalink",        null: false
+    t.string   "title",            null: false
+    t.boolean  "published",        null: false
+    t.datetime "published_at",     null: false
+    t.string   "markup_lang",      null: false
+    t.text     "raw_content",      null: false
+    t.text     "html_content",     null: false
+    t.text     "html_overview"
+    t.string   "tags_string"
+    t.string   "meta_description", null: false
+    t.string   "meta_image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blogo_posts", ["permalink"], name: "index_blogo_posts_on_permalink", unique: true, using: :btree
+  add_index "blogo_posts", ["published_at"], name: "index_blogo_posts_on_published_at", using: :btree
+  add_index "blogo_posts", ["user_id"], name: "index_blogo_posts_on_user_id", using: :btree
+
+  create_table "blogo_taggings", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id",  null: false
+  end
+
+  add_index "blogo_taggings", ["tag_id", "post_id"], name: "index_blogo_taggings_on_tag_id_and_post_id", unique: true, using: :btree
+
+  create_table "blogo_tags", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blogo_tags", ["name"], name: "index_blogo_tags_on_name", unique: true, using: :btree
+
+  create_table "blogo_users", force: :cascade do |t|
+    t.string   "name",            null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blogo_users", ["email"], name: "index_blogo_users_on_email", unique: true, using: :btree
+
+  create_table "monologue_posts", force: :cascade do |t|
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.string   "url"
+    t.datetime "published_at"
+  end
+
+  add_index "monologue_posts", ["url"], name: "index_monologue_posts_on_url", unique: true, using: :btree
+
+  create_table "monologue_taggings", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
+
+  add_index "monologue_taggings", ["post_id"], name: "index_monologue_taggings_on_post_id", using: :btree
+  add_index "monologue_taggings", ["tag_id"], name: "index_monologue_taggings_on_tag_id", using: :btree
+
+  create_table "monologue_tags", force: :cascade do |t|
+    t.string "name"
+  end
+
+  add_index "monologue_tags", ["name"], name: "index_monologue_tags_on_name", using: :btree
+
+  create_table "monologue_users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "spina_accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "postal_code"
+    t.string   "city"
+    t.string   "phone"
+    t.string   "email"
+    t.text     "preferences"
+    t.string   "logo"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "kvk_identifier"
+    t.string   "vat_identifier"
+    t.boolean  "robots_allowed", default: false
+  end
+
+  create_table "spina_attachment_collections", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "spina_attachment_collections_attachments", force: :cascade do |t|
+    t.integer "attachment_collection_id"
+    t.integer "attachment_id"
+  end
+
+  create_table "spina_attachments", force: :cascade do |t|
+    t.string   "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "spina_colors", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "spina_layout_parts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "name"
+    t.integer  "layout_partable_id"
+    t.string   "layout_partable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "account_id"
+  end
+
+  create_table "spina_line_translations", force: :cascade do |t|
+    t.integer  "spina_line_id", null: false
+    t.string   "locale",        null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "content"
+  end
+
+  add_index "spina_line_translations", ["locale"], name: "index_spina_line_translations_on_locale", using: :btree
+  add_index "spina_line_translations", ["spina_line_id"], name: "index_spina_line_translations_on_spina_line_id", using: :btree
+
+  create_table "spina_lines", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "spina_page_parts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "name"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "page_id"
+    t.integer  "page_partable_id"
+    t.string   "page_partable_type"
+  end
+
+  create_table "spina_page_translations", force: :cascade do |t|
+    t.integer  "spina_page_id",     null: false
+    t.string   "locale",            null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "title"
+    t.string   "menu_title"
+    t.string   "description"
+    t.string   "seo_title"
+    t.string   "materialized_path"
+  end
+
+  add_index "spina_page_translations", ["locale"], name: "index_spina_page_translations_on_locale", using: :btree
+  add_index "spina_page_translations", ["spina_page_id"], name: "index_spina_page_translations_on_spina_page_id", using: :btree
+
+  create_table "spina_pages", force: :cascade do |t|
+    t.string   "title"
+    t.string   "menu_title"
+    t.string   "description"
+    t.boolean  "show_in_menu",        default: true
+    t.string   "slug"
+    t.boolean  "deletable",           default: true
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "seo_title"
+    t.boolean  "skip_to_first_child", default: false
+    t.string   "view_template"
+    t.string   "layout_template"
+    t.boolean  "draft",               default: false
+    t.string   "link_url"
+    t.string   "ancestry"
+    t.integer  "position"
+    t.string   "materialized_path"
+    t.boolean  "active",              default: true
+  end
+
+  create_table "spina_photo_collections", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "spina_photo_collections_photos", force: :cascade do |t|
+    t.integer "photo_collection_id"
+    t.integer "photo_id"
+    t.integer "position"
+  end
+
+  create_table "spina_photos", force: :cascade do |t|
+    t.string   "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "spina_rewrite_rules", force: :cascade do |t|
+    t.string   "old_path"
+    t.string   "new_path"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "spina_structure_items", force: :cascade do |t|
+    t.integer  "structure_id"
+    t.integer  "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "spina_structure_items", ["structure_id"], name: "index_spina_structure_items_on_structure_id", using: :btree
+
+  create_table "spina_structure_parts", force: :cascade do |t|
+    t.integer  "structure_item_id"
+    t.integer  "structure_partable_id"
+    t.string   "structure_partable_type"
+    t.string   "name"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "spina_structure_parts", ["structure_item_id"], name: "index_spina_structure_parts_on_structure_item_id", using: :btree
+  add_index "spina_structure_parts", ["structure_partable_id"], name: "index_spina_structure_parts_on_structure_partable_id", using: :btree
+
+  create_table "spina_structures", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "spina_text_translations", force: :cascade do |t|
+    t.integer  "spina_text_id", null: false
+    t.string   "locale",        null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.text     "content"
+  end
+
+  add_index "spina_text_translations", ["locale"], name: "index_spina_text_translations_on_locale", using: :btree
+  add_index "spina_text_translations", ["spina_text_id"], name: "index_spina_text_translations_on_spina_text_id", using: :btree
+
+  create_table "spina_texts", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "spina_users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "admin",           default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.datetime "last_logged_in"
   end
 
 end
